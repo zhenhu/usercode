2fae02090a9da22a4d474e031dd6aff797c0029e
Zhen <zhen.hu@cern.ch>
2015-10-15 22:41:15 -0500

    update for 1/fb

--- Onia/test/runOnia2MuMuRootupler.py
+++ Onia/test/runOnia2MuMuRootupler.py
@@ -7,11 +7,11 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 500
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:/eos/uscms/store/user/zhenhu/MuOnia/Onia2MuMuPAT-Run2015D-MuOnia-v1/151013_131000/0000/Onia2MuMuPAT_200.root')
+   fileNames = cms.untracked.vstring('file:/eos/uscms/store/user/zhenhu/MuOnia/Onia2MuMuPAT-Run2015D-MuOnia-v1/151013_131000/0000/Onia2MuMuPAT_200.root')
 )
 
 process.TFileService = cms.Service("TFileService",
-        fileName = cms.string('Rootuple.root'),
+   fileName = cms.string('Rootuple.root'),
 )
 
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True))
